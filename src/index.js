61c7be8d4e3b22d4b76ee2528771e2373bcacd1c
DinkoSt <114727440+DinkoSt@users.noreply.github.com>
2022-10-07 18:14:44 +0300

    problem submit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 let express = require('express');
+let handlebars = require('express-handlebars');
 
 
 let app = express();
+app.use(express.static('publick'));
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+
+app.set('view engine','hbs');
+app.set('views', './src/views')
 
 app.get('/', (req,res) => {
-    res.send('Hello World')
+    res.render('index')
 })
 
 app.listen(3000, () => console.log(`App is listening on port 5000`))
